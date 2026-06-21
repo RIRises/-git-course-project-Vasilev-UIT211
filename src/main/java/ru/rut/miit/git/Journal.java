a40a7a1e97d6dd8e2fdabba80e43df16d2ba9f08
cachy <ilyawork173@gmail.com>
2026-06-21 20:19:26 +0300

    fix: Убрать skip(1), ломающий пустые файлы

--- src/main/java/ru/rut/miit/git/Journal.java
+++ src/main/java/ru/rut/miit/git/Journal.java
@@ -60,7 +60,8 @@ public class Journal {
             return Collections.emptyList();
         }
 
-        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8).stream().skip(1).collect(Collectors.toList());
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8).stream().collect(Collectors.toList());
+        //Убрал skip(1) тк это не имеет логики и при пустой строке выдается ошибка
         System.out.println("--- Записи дневника ---");
         lines.forEach(System.out::println);
         System.out.println("-----------------------");
